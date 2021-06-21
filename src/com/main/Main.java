0204a59d95fe0a0ae7f4b6368dcba2035a001930
Akram Dhaifullah <akramdhaifullah@webmail.umm.ac.id>
2021-06-21 11:10:21 +0800

    Initial commit: Project initialization with NetBeans IDE.

---
+++ src/com/main/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.main;
+
+/**
+ *
+ * @author Akram Dhaifullah
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Does NetBeans work with Intellij?");
+    }
+    
+}
